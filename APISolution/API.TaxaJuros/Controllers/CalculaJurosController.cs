39b56fa67831d40da96994e9f23e4e500901a18f
Marcela Mariotti Peres <marcelamperes@gmail.com>
2021-01-29 12:18:24 -0300

    subindo arquitetura base no git

---
+++ APISolution/API.TaxaJuros/Controllers/CalculaJurosController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    public class CalculaJurosController : ControllerBase
+    {
+        [HttpGet]
+        [Route("calculajuros")]
+        /// <summary>
+        /// Método que retorna o cálculo
+        /// </summary>
+        /// <returns>cálculo</returns>
+        public decimal GetCalculoJuros(decimal valorinicial, int meses)
+        {
+            //Valor Final = Valor Inicial *(1 + juros) ^ Tempo
+            //Valor inicial é um decimal recebido como parâmetr
+            //Valor do Juros deve ser consultado na API 1
+            //Tempo é um inteiro, que representa meses, também recebido como parâmetro
+            //^ representa a operação de potência
+            //Resultado final deve ser truncado (sem arredondamento) em duas casas decimais
+            //Exemplo: /calculajuros?valorinicial=100&meses=5 Resultado esperado: 105,10
+
+
+            decimal juros = Convert.ToDecimal(0.01);
+            return juros;
+        }
+
+        [HttpGet]
+        [Route("showmethecode")]
+        /// <summary>
+        /// Método que retorna a URL do código fonte do projeto no Git
+        /// </summary>
+        /// <returns>url git</returns>
+        public string GetUrlCode()
+        {
+            return "https://github.com/marcelamperes/api-calculajuros";
+        }
+    }
+}
---
+++ APISolution/API.TaxaJuros/Controllers/TaxaJurosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [ApiController]
+    public class TaxaJurosController : ControllerBase
+    {
+        [HttpGet]
+        [Route("taxaJuros")]
+        /// <summary>
+        /// Método que retorna o valor do juros
+        /// </summary>
+        /// <returns>juros</returns>
+        public decimal GetTaxaJuros()
+        {
+            decimal juros = Convert.ToDecimal(0.01);
+            return juros;
+        }
+    }
+}
